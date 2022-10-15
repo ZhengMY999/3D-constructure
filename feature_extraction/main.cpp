4cc56c3bbc02f485cb5f88db1bca600e5720b861
ZhengMY999 <91540630+ZhengMY999@users.noreply.github.com>
2022-10-15 16:49:00 +0800

    SearchInLine

--- feature_extraction/main.cpp
+++ feature_extraction/main.cpp
@@ -38,8 +38,25 @@ int main(int argc, char* argv[])
     imshow("mat2", mat2);
 
     //交点进行匹配
-    surf(image_R, image_L, mat1, mat2);
+    //surf(image_R, image_L, mat1, mat2);
+     
+    //10.15 添加
+    //获取交点
+    Mat pointR = get_joints(image_R, 8, 8);
+    Mat pointL = get_joints(image_L, 8, 8);
+    //imshow("point1", point1);
+    //imshow("point2", point2);
 
+
+    //获取横竖骨架
+    Mat horizontal_L;
+    Mat vertical_L;
+    Mat horizontal_R;
+    Mat vertical_R;
+    get_skeleton(image_L, 5, 5, horizontal_L, vertical_L);
+    get_skeleton(image_R, 5, 5, horizontal_R, vertical_R);
+    line_search(image_L, image_R, pointL, pointR, horizontal_L, vertical_L, horizontal_R, vertical_R);
+    
     cv::waitKey(0);
     return 0;
 }
